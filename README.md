363ac35b40e376d1289a8ebf10eaebe874ffb988
Nathan Lin <nathanlukelin@gmail.com>
2021-10-18 09:43:54 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Mathey
+
+To run your code, go to the [MyMain.java](src/main/java/MyMain.java) and file and click on `Run` above the `main()` method.
+
+## Run your tests with:
+```shell script
+make test
+```
---
+++ src/main/java/MyMain.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class MyMain {
+
+    public static int someMethod() {
+        return 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+}
+
+
